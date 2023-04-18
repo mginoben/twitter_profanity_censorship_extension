bde00de4742244ce8fc12e473f0e9009d9c38994
Gino Ben R. Magsino <mginoben@gmail.com>
2023-04-18 18:30:53 +0800

    Added changes

--- background.js
+++ background.js
@@ -9,12 +9,28 @@ chrome.runtime.onMessage.addListener((response, sender, sendResponse) => {
     // Stop the loading animation by setting the badge text to ''
     chrome.action.setBadgeBackgroundColor({ color: "#8b0000", tabId: sender.tab.id });
 
-    let frames = ["-", "\\", "|", "/"];
+    let countdown = response.estimatedTime; // Set the initial value of the countdown timer
 
-    for (let i = 0; i < frames.length; i++) {
-      const frame = frames[i];
-      chrome.browserAction.setBadgeText({text: frame, tabId: sender.tab.id });
-    }
+    // Set the badge text to the initial value of the countdown timer
+    chrome.browserAction.setBadgeText({text: countdown.toString()});
+
+    // Use setInterval() to update the badge text every second
+    let countdownInterval = setInterval(function() {
+      countdown--;
+      if (countdown === 0) {
+        clearInterval(countdownInterval);
+        if (abusiveCount > 0) {
+          chrome.action.setBadgeText({text: abusiveCount.toString(), tabId: sender.tab.id });
+          chrome.action.setBadgeTextColor({ color: '#ffffff', tabId: sender.tab.id  });
+          chrome.action.setBadgeBackgroundColor({ color: "#5A5A5A", tabId: sender.tab.id });
+        }
+        else {
+          chrome.action.setBadgeText({text: "", tabId: sender.tab.id });
+        }
+      } else {
+        chrome.browserAction.setBadgeText({text: countdown.toString()});
+      }
+    }, 1000);
 
   }
 
@@ -37,9 +53,22 @@ chrome.runtime.onMessage.addListener((response, sender, sendResponse) => {
   }
   
   else if (response.action == "push") { // ADD ABUSIVE TWEET
+
     const data = response.data;
-    tweetPredictions.push(data);
-    console.log(data);  
+    let foundTweet = false;
+
+    for (let i = 0; i < tweetPredictions.length; i++) {
+      if (tweetPredictions[i]["tweet"] === data.tweet) {
+        foundTweet = true;
+        break;
+      }
+    }
+
+    if (!foundTweet) {
+      tweetPredictions.push(data);
+      console.log(data);  
+    }
+
   }
 
   else if (response.action == "get") { // GET ALL TWEETS (ABUSIVE/UNCENSORED)
@@ -57,9 +86,12 @@ chrome.runtime.onMessage.addListener((response, sender, sendResponse) => {
 
 // Listen for updates to the active tab
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-
-  console.log("BRUH");
-  // tweetPredictions.length = 0;
-  
+  // Only send message when the tab has finished loading
+  if (changeInfo.status === 'loading') {
+    // Send message to the content script
+    chrome.tabs.sendMessage(tabId, { message: 'TabUpdated' }, response => {
+      console.log(response);
+    });
+  }
 });
 
--- popup.js
+++ popup.js
@@ -19,20 +19,49 @@ async function query(tweet) {
         
 
     if (response.ok) {
-        return response.json();
+        return await response.json();
     } else {
         console.log(`HTTP Response Code:`, response.status);
         return "error";
     }
         
 }
-// const preds = query("Tangina mo");
-// console.log(query("Tangina mo").then(response => {return response;}));
+
+
+
+    const tweets = document.querySelectorAll(".censored");
+
+    tweets.forEach(tweetDiv => {
+        tweetDiv.addEventListener('click', function(event) {
+
+            if (tweetDiv.classList.contains("show")) {
+    
+                const element = event.target;
+    
+                if (tweetDiv.innerText != element.innerText && element.innerText == "Show more") {
+                    tweetDiv.classList.remove("show");
+                }
+                else {
+                    tweetDiv.classList.remove("show");
+                    event.stopPropagation();
+                }
+    
+            } 
+            else if (!tweetDiv.classList.contains("show")){ 
+        
+                tweetDiv.classList.add("show");
+                event.stopPropagation();
+                
+            }
+    
+        });
+    });
+
 
 function censor(tweetDiv) {
 
-    tweetDiv.classList.add("censored"); ;
-    
+    tweetDiv.classList.add("censored");
+
     tweetDiv.addEventListener('click', function(event) {
 
         if (tweetDiv.classList.contains("show")) {
@@ -40,7 +69,6 @@ function censor(tweetDiv) {
             const element = event.target;
 
             if (tweetDiv.innerText != element.innerText && element.innerText == "Show more") {
-                console.log(element.innerText);
                 tweetDiv.classList.remove("show");
             }
             else {
@@ -49,7 +77,7 @@ function censor(tweetDiv) {
             }
 
         } 
-        else { 
+        else if (!tweetDiv.classList.contains("show")){ 
     
             tweetDiv.classList.add("show");
             event.stopPropagation();
@@ -57,118 +85,152 @@ function censor(tweetDiv) {
         }
 
     });
-        
-    // }
+   
 
 }
 
 function getUsername(tweet) {
+
     const tweetUsername = tweet.replace(/[\r\n]/gm, ' ').split(' ');
-    let username;
+   
     tweetUsername.forEach(word => {
+       
         if (word.includes('@')) {
-            username = word;
+           word_split = word.split('·');
+           if (word_split.length > 1) {
+                return word_split[0];
+           }
+            return word;
         }
+
     });
-    return username;
+
 }
+
 
 function getTweets() {
 
-    let newTweetFound = false;
+    let executionInterval = 100; 
+
     const tweets = document.querySelectorAll('article[data-testid="tweet"]');
 
     for (let i = 0; i < tweets.length; i++) { 
 
-        chrome.runtime.sendMessage({action: "get"}, function(response) {
+        var tweetDivs = tweets[i].querySelectorAll('[data-testid="tweet"] [lang]');
 
-            var tweetDiv = tweets[i].querySelector('[data-testid="tweet"] [lang]');
+        for (let j = 0; j < tweetDivs.length; j++) {
+
+            const tweetDiv = tweetDivs[j];
             var usernameDiv = tweets[i].querySelector('[data-testid="User-Name"]');
             const language = tweetDiv.getAttribute("lang");
-            const savedTweets = response.tweets;
-            const tweet = tweetDiv.innerText;
+            const tweet = tweetDiv.innerText.replace(/[\r\n]/gm, ' ');
             const username = getUsername(usernameDiv.innerText);
 
             if (language != "tl") {
-                // console.log("Not Tagalog", tweet);
                 return;
             }
 
-            if (!savedTweets.includes(tweet)) {
+            query(tweet).then(result => {
 
-                newTweetFound = true;
-    
-                query(tweet).then(result => {
-                    
-                    if (result == "error") { // ERROR
-                        console.log("Error");
-                        return; 
-                    } 
-    
-                    const prediction = result["data"][0]["label"];
-    
-                    if (prediction == "Model is loading. Try again.") { // LOADING
-                        console.log("Model Loading");
-                        chrome.runtime.sendMessage({ 
-                            status: "loading",
-                        });
-                        return; 
-                    }
-        
-                    // GOOD
-                    const data = { tweet, username, prediction };
-                        
-                    chrome.runtime.sendMessage({ 
-                        action: "push",
-                        data: data
-                    });
+                chrome.runtime.sendMessage({action: "get"}, function(response) {
 
-                    if (prediction == "Abusive") {
-                        censor(tweetDiv);                
-                    }
+                    const savedTweets = response.tweets;
 
-                    // console.log(tweetDiv.getAttribute("lang"));
-                    
-                                        
-                }); 
+                    if (!savedTweets.includes(tweet)) {
+                            
+                            // console.log(result);
+
+                            if (result == "error") { return; } 
+                            
+                            const prediction = result["data"][0]["label"];
+
+                            console.log(prediction);
+
+                            if (prediction == "Loading Model (Estimated Time: 20.0 Seconds)") {
+                                executionInterval = 20000;
+                                return; 
+                            }
                 
-            }
-            else {
-                chrome.runtime.sendMessage({ action: "compare", tweet: tweet }, function(response) {
+                            // GOOD
+                            
+                            const data = { tweet, username, prediction };
+                            
 
-                    if (response.result.prediction == "Abusive" && !tweetDiv.classList.contains("censored")) {
-                        censor(tweetDiv);
+                            chrome.runtime.sendMessage({action: "get"}, function(response) {
+                                if (!response.tweets.includes(tweet)) {
+                                    chrome.runtime.sendMessage({ 
+                                        action: "push",
+                                        data: data
+                                    });
+                                }
+                            });
+                    
+                                                
+                        
+                    }
+                    else {
+                        chrome.runtime.sendMessage({ action: "compare", tweet: tweet }, function(response) {
+
+                            if (response.result.prediction == "Abusive" && !tweetDiv.classList.contains("censored")) {
+                                censor(tweetDiv);
+                            }
+
+                        });
                     }
 
+
+                    chrome.runtime.sendMessage({ 
+                        status: "running"
+                    });
                 });
-            }
+                
+            }); 
+                
+                
+                
+            
 
-
-            chrome.runtime.sendMessage({ 
-                status: "running"
-            });
-        });
-
-        // if (newTweetFound) {
-        //     break;
-        // }
+        }
 
     }
 
-    return newTweetFound;
+    return executionInterval;
 
-}
-
-function checkTweets() {
-    
 }
 
 
 window.onload = function() {
 
-    var intervalId = setInterval(function() {
-        
-        getTweets();
+    document.querySelector('body').style.display = 'none';
 
+    setTimeout(function() {
+        document.querySelector('body').style.display = 'block';
     }, 1000);
+
+    let interval = 100;
+    let running = false;
+    
+    setInterval(function() {
+    
+        if (!running) {
+            running = true;
+            interval = getTweets(interval);
+            running = false;
+            
+        }
+    
+    }, interval);
+
+    // Listen for message from background.js
+    // chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    //     if (request.message === 'TabUpdated') {
+    //         console.log("WEW");
+    //         document.querySelector('body').style.display = 'none';
+
+    //         setTimeout(function() {
+    //             document.querySelector('body').style.display = 'block';
+    //         }, 1000);
+    //     }
+    // });
+    
 };
